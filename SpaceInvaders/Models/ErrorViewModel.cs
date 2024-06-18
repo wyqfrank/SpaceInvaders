84312938c79e8b93accf326d0d77c514a6634f15
wyqfrank <134373790+wyqfrank@users.noreply.github.com>
2024-06-18 18:45:48 +1000

    Add project files.

---
+++ SpaceInvaders/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpaceInvaders.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
