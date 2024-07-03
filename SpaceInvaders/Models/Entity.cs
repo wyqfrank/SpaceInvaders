d9152926db4018d2c8089932ce4c8fdb82f9fd9a
numnum5 <127462339+numnum5@users.noreply.github.com>
2024-07-03 15:31:03 +1000

    Entity

---
+++ SpaceInvaders/Models/Entity.cs
@@ -0,0 +1,32 @@
+﻿namespace SpaceInvaders.Models
+{
+    public abstract class Entity
+    {
+        private int[,] position;
+        private int health;
+
+        public Entity(int[,] position, int health) 
+        {
+            this.position = position;
+            this.health = health;
+        }
+
+        public int[,] getPosition()
+        {
+            return this.position;
+        }
+
+        public int getHealth(int health)
+        {
+            return this.health;
+        }
+        public void setPosition(int[,] newPosition)
+        {
+            this.position = newPosition; 
+        }
+        public void setHealth(int health)
+        {
+            this.health = health;
+        }
+    }
+}
