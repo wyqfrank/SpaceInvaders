47e81398408468d81d7852352acfce35df6d0a9f
wyqfrank <134373790+wyqfrank@users.noreply.github.com>
2024-07-07 19:19:43 +1000

    Game of Life mob pattern

---
+++ SpaceInvaders/Models/GoL.cs
@@ -0,0 +1,27 @@
+﻿namespace SpaceInvaders.Models
+{
+    public class GoL
+    {
+        GameMap gameMap = null!;
+        Player[] players = null!;
+        Mob[] mobs = null!;
+
+        public GoL(GameMap gameMap)
+        {
+            this.gameMap = gameMap;
+        }
+
+        public void evolve()
+        {
+            this.gameMap.GetMapLayout();
+        }
+
+        public void count_neighbours()
+        {
+            char [][] layout = this.gameMap.GetMapLayout();
+            
+        }
+
+
+    }
+}
